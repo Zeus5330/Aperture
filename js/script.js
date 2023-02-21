4dce7bfeb5a05194f45e2b53b6717a34c0cee681
Zeus5330 <Dmitry703@bigmir.net>
2023-02-21 16:23:17 +0200

    connect_ui_dialog_berger_menu

--- js/script.js
+++ js/script.js
@@ -16,3 +16,68 @@ $(document).ready(function(){
         }
     });
 });
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
+
+
+
+
+
+(function($) { "use strict";
+
+    //Page cursors
+
+
+    //Navigation
+
+    var app = function () {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+        var init = function init() {
+            body = document.querySelector('body');
+            menu = document.querySelector('.menu-icon');
+            menuItems = document.querySelectorAll('.nav__list-item');
+            applyListeners();
+        };
+        var applyListeners = function applyListeners() {
+            menu.addEventListener('click', function () {
+                return toggleClass(body, 'nav-active');
+            });
+        };
+        var toggleClass = function toggleClass(element, stringClass) {
+            if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+        };
+        init();
+    }();
+
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("light")) {
+            $("body").removeClass("light");
+            $("#switch").removeClass("switched");
+        }
+        else {
+            $("body").addClass("light");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
